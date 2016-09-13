d360091fbbcb70db71f5c6c1802ec7bd53627396
Jhan Rojas <jhanrojasbuitron@gmail.com>
2016-09-13 14:22:28 +0000

    custom page after sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,21 @@
 class SessionsController < Devise::SessionsController
+    
+    def after_sign_in_path_for(resource) 
+    
+    pages_home_path
+    
+    end 
+    
+    def after_sign_up_path_for(resource) 
+    
+    pages_home_path
+    
+    end 
+    
+    def after_update_path_for(resource) 
+    
+    pages_home_path
+    
+    end 
 
 end
