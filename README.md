3da707d555f33832754b495706d2bf054ca2d105
Jhan Rojas <jhanrojasbuitron@gmail.com>
2016-09-26 06:21:10 +0000

    edit devise and README

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # gotravel-restful
 
 This project contains the main web page and the rest service.
+
+[![Run in Postman](https://run.pstmn.io/button.svg)](https://app.getpostman.com/run-collection/75a8deb5b81e4154a637)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!, :except => [:index]
+  skip_before_action :verify_authenticity_token
 end
