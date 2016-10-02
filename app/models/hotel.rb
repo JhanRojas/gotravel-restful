08dda829dc63c49947facddaa5c61a6e09f9d456
Jhan Rojas <jhanrojasbuitron@gmail.com>
2016-10-02 09:15:49 +0000

    adding validation required for hotel

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,4 +1,12 @@
 class Hotel < ActiveRecord::Base
   belongs_to :location
   has_many :rooms
+  
+  validates :name, presence: true
+  validates :starts, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
+  validates :country, presence: true
+  validates :state, presence: true
+  validates :city, presence: true
 end
