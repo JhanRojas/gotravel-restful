44ea97a4115ece22188aa669c732efa8750c35a3
joli804 <joli804@hotmail.com>
2016-10-02 06:08:11 +0000

    incluyendo paquete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161002030845) do
+ActiveRecord::Schema.define(version: 20161002050205) do
 
   create_table "airlines", force: :cascade do |t|
     t.string   "name",        limit: 255
