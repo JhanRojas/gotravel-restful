2f42c1162244fdc5a7fdd61f4658596d5a2a8bcf
Jhan Rojas <jhanrojasbuitron@gmail.com>
2016-10-06 19:33:20 +0000

    adding mysql start

--- restart_db.sh
+++ restart_db.sh
@@ -1,4 +1,10 @@
+echo "service mysql start"
+sudo service mysql start
+echo "rake db:drop"
 rake db:drop
+echo "rake db:create"
 rake db:create
+echo "rake db:migrate"
 rake db:migrate
+echo "rake db:seed"
 rake db:seed
